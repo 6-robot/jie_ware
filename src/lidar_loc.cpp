a8ec9a3fb92b2bac9989dfe24048bcf35d3d10f7
Robot <zhangwanjie@126.com>
2024-10-12 17:10:51 +0800

    improve the strategy

--- src/lidar_loc.cpp
+++ src/lidar_loc.cpp
@@ -168,8 +168,7 @@ void crop_map()
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::vector<cv::Point2f> scan_recv;
-    // scan_points.clear();
+    scan_points.clear();
     double angle = msg->angle_min;
     for (size_t i = 0; i < msg->ranges.size(); ++i)
     {
@@ -177,11 +176,86 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
         {
             float x = msg->ranges[i] * cos(angle) / map_msg.info.resolution;
             float y = -msg->ranges[i] * sin(angle) / map_msg.info.resolution;
-            scan_recv.push_back(cv::Point2f(x, y));
+            scan_points.push_back(cv::Point2f(x, y));
         }
         angle += msg->angle_increment;
     }
-    scan_points = scan_recv;
+
+    while (ros::ok())
+    {
+        if (!map_cropped.empty())
+        {
+            map_match = map_temp.clone();
+            
+            // 计算三种情况下的雷达点坐标数组
+            std::vector<cv::Point2f> transform_points, clockwise_points, counter_points;
+            float deg_to_rad = M_PI / 180.0;
+            int max_sum = 0;
+            float best_dx = 0, best_dy = 0, best_dyaw = 0;
+
+            for (const auto& point : scan_points)
+            {
+                // 情况一：原始角度
+                float rotated_x = point.x * cos(lidar_yaw) - point.y * sin(lidar_yaw);
+                float rotated_y = point.x * sin(lidar_yaw) + point.y * cos(lidar_yaw);
+                transform_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
+
+                // 情况二：顺时针旋转1度
+                float clockwise_yaw = lidar_yaw + deg_to_rad;
+                rotated_x = point.x * cos(clockwise_yaw) - point.y * sin(clockwise_yaw);
+                rotated_y = point.x * sin(clockwise_yaw) + point.y * cos(clockwise_yaw);
+                clockwise_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
+
+                // 情况三：逆时针旋转1度
+                float counter_yaw = lidar_yaw - deg_to_rad;
+                rotated_x = point.x * cos(counter_yaw) - point.y * sin(counter_yaw);
+                rotated_y = point.x * sin(counter_yaw) + point.y * cos(counter_yaw);
+                counter_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
+            }
+
+            // 计算15种变换方式的匹配值
+            std::vector<cv::Point2f> offsets = {{0,0}, {1,0}, {-1,0}, {0,1}, {0,-1}};
+            std::vector<std::vector<cv::Point2f>> point_sets = {transform_points, clockwise_points, counter_points};
+            std::vector<float> yaw_offsets = {0, deg_to_rad, -deg_to_rad};
+
+            for (int i = 0; i < offsets.size(); ++i)
+            {
+                for (int j = 0; j < point_sets.size(); ++j)
+                {
+                    int sum = 0;
+                    for (const auto& point : point_sets[j])
+                    {
+                        float px = point.x + offsets[i].x;
+                        float py = point.y + offsets[i].y;
+                        if (px >= 0 && px < map_temp.cols && py >= 0 && py < map_temp.rows)
+                        {
+                            sum += map_temp.at<uchar>(py, px);
+                        }
+                    }
+                    if (sum > max_sum)
+                    {
+                        max_sum = sum;
+                        best_dx = offsets[i].x;
+                        best_dy = offsets[i].y;
+                        best_dyaw = yaw_offsets[j];
+                    }
+                }
+            }
+
+            // 更新雷达位置和角度
+            lidar_x += best_dx;
+            lidar_y += best_dy;
+            lidar_yaw += best_dyaw;
+
+            if(best_dx == 0 && best_dy == 0 && best_dyaw == 0)
+            {
+                break;
+            }
+
+        }
+
+    }
+
     if(clear_countdown > -1)
         clear_countdown --;
     if(clear_countdown == 0)
@@ -322,103 +396,15 @@ int main(int argc, char** argv)
     ros::Subscriber initial_pose_sub = nh.subscribe("/initialpose", 1, initialPoseCallback);
     clear_costmaps_client = nh.serviceClient<std_srvs::Empty>("/move_base/clear_costmaps");
 
-    ros::Rate rate(100);  // 匹配频率
+    ros::Rate rate(30);  // tf发送频率
 
     while (ros::ok())
     {
-        if (!map_cropped.empty())
-        {
-            map_match = map_temp.clone();
-            // cv::cvtColor(map_match, map_match, cv::COLOR_GRAY2BGR);
-
-            // 遍历 map_cropped，将 map_match 中对应的障碍物像素设置为紫色
-            // for (int y = 0; y < map_cropped.rows; ++y)
-            // {
-            //     for (int x = 0; x < map_cropped.cols; ++x)
-            //     {
-            //         if (map_cropped.at<uchar>(y, x) == 100)  //  100 表示障碍物
-            //         {
-            //             map_match.at<cv::Vec3b>(y, x) = cv::Vec3b(128, 0, 128);  // 紫色 (BGR格式)
-            //         }
-            //     }
-            // }
-
-            // 计算三种情况下的雷达点坐标数组
-            std::vector<cv::Point2f> transform_points, clockwise_points, counter_points;
-            float deg_to_rad = M_PI / 180.0;
-            int max_sum = 0;
-            float best_dx = 0, best_dy = 0, best_dyaw = 0;
-
-            for (const auto& point : scan_points)
-            {
-                // 情况一：原始角度
-                float rotated_x = point.x * cos(lidar_yaw) - point.y * sin(lidar_yaw);
-                float rotated_y = point.x * sin(lidar_yaw) + point.y * cos(lidar_yaw);
-                transform_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
-
-                // 情况二：顺时针旋转1度
-                float clockwise_yaw = lidar_yaw + deg_to_rad;
-                rotated_x = point.x * cos(clockwise_yaw) - point.y * sin(clockwise_yaw);
-                rotated_y = point.x * sin(clockwise_yaw) + point.y * cos(clockwise_yaw);
-                clockwise_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
-
-                // 情况三：逆时针旋转1度
-                float counter_yaw = lidar_yaw - deg_to_rad;
-                rotated_x = point.x * cos(counter_yaw) - point.y * sin(counter_yaw);
-                rotated_y = point.x * sin(counter_yaw) + point.y * cos(counter_yaw);
-                counter_points.push_back(cv::Point2f(rotated_x + lidar_x, lidar_y - rotated_y));
-            }
-
-            // 计算15种变换方式的匹配值
-            std::vector<cv::Point2f> offsets = {{0,0}, {1,0}, {-1,0}, {0,1}, {0,-1}};
-            std::vector<std::vector<cv::Point2f>> point_sets = {transform_points, clockwise_points, counter_points};
-            std::vector<float> yaw_offsets = {0, deg_to_rad, -deg_to_rad};
-
-            for (int i = 0; i < offsets.size(); ++i)
-            {
-                for (int j = 0; j < point_sets.size(); ++j)
-                {
-                    int sum = 0;
-                    for (const auto& point : point_sets[j])
-                    {
-                        float px = point.x + offsets[i].x;
-                        float py = point.y + offsets[i].y;
-                        if (px >= 0 && px < map_temp.cols && py >= 0 && py < map_temp.rows)
-                        {
-                            sum += map_temp.at<uchar>(py, px);
-                        }
-                    }
-                    if (sum > max_sum)
-                    {
-                        max_sum = sum;
-                        best_dx = offsets[i].x;
-                        best_dy = offsets[i].y;
-                        best_dyaw = yaw_offsets[j];
-                    }
-                }
-            }
-
-            // 更新雷达位置和角度
-            lidar_x += best_dx;
-            lidar_y += best_dy;
-            lidar_yaw += best_dyaw;
-
-            // 绘制最佳匹配的测距点
-            // for (const auto& point : transform_points)
-            // {
-            //     float px = point.x + best_dx;
-            //     float py = point.y + best_dy;
-            //     if (px >= 0 && px < map_match.cols && py >= 0 && py < map_match.rows)
-            //     {
-            //         map_match.at<cv::Vec3b>(py, px) = cv::Vec3b(0, 0, 255);  // BGR 格式：红色
-            //     }
-            // }
-        }
-
         pose_tf();
         ros::spinOnce();
         rate.sleep();
     }
+
 
     return 0;
 }
